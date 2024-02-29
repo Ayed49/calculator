fe786af9585fd1c3ee0aaa147a31f5c662ed7826
Ayed49 <ayedbabu60@gmail.com>
2024-02-29 17:04:17 +0600

    first commit

---
+++ calculator.js
@@ -0,0 +1,7 @@
+    const input=document.getElementById('input-box');
+    const buttons=document.querySelectorAll('button');
+
+    const buttonsArray = Array.from(buttons);
+
+
+    console.log(buttons);
