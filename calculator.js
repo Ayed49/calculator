07656171525e562036266d959fbe461333948f2b
Ayed49 <ayedbabu60@gmail.com>
2024-02-29 18:12:44 +0600

    first commit

--- calculator.js
+++ calculator.js
@@ -5,12 +5,24 @@ let string = '';
 const buttonsArray = Array.from(buttons);
 buttonsArray.forEach(btn => {
     btn.addEventListener('click', (e) => {
+        if (e.target.innerHTML == 'DEL') {
+            string = string.substring(0, string.length - 1);
+            display.value = string;
+        }
+        else if (e.target.innerHTML == 'AC') {
+            string = '';
+            display.value = string;
+        } else if (e.target.innerHTML == '=') {
+            string = eval(string);
+            display.value = string;
+        }
 
+        else {
+            string += e.target.innerHTML;
+            display.value = string;
 
-        
-        string += e.target.innerHTML;
-        display.value = string;
-        console.log(e.target.innerHTML);
+        }
+
     })
 })
 
