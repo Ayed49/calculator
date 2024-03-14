2fd1e3de696d0e33918d7fb4f041c07010c13140
Ayed49 <ayedbabu60@gmail.com>
2024-03-14 15:17:16 +0600

    first commit

--- argument.js
+++ argument.js
@@ -1,21 +1,26 @@
-function addNumber(num1, num2){
-    let sum= 0;
-    for(const num of arguments){
-        sum= sum + num;
+function addNumber(num1, num2) {
+    let sum = 0;
+    for (const num of arguments) {
+        sum = sum + num;
     }
-    
+
     return sum;
 }
-const sum= addNumber(23, 13, 50, 100);
+const sum = addNumber(23, 13, 50, 100);
 // console.log(sum);
 
 
-function getFullName(firstName, lastName){
-    let fullName='';
-    for(const part of arguments){
-        fullName= fullName+part+' ';
+function getFullName(firstName, lastName) {
+    let fullName = '';
+    for (const part of arguments) {
+        fullName = fullName + part + ' ';
     }
     return fullName;
 }
-const fullName= getFullName('go', 'hu', 'lo', 'koo');
+const fullName = getFullName('go', 'hu', 'lo', 'koo');
 console.log(fullName);
+
+
+const add = (num1, num2) => num1 + num2;
+const ans = add(20, 30);
+console.log(ans);
