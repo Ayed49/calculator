49d28e6144c297965f1a752aa35053ff3dcf812b
Ayed49 <ayedbabu60@gmail.com>
2024-03-14 15:34:44 +0600

    first commit

--- argument.js
+++ argument.js
@@ -1,26 +1,36 @@
-function addNumber(num1, num2) {
-    let sum = 0;
-    for (const num of arguments) {
-        sum = sum + num;
-    }
+// function addNumber(num1, num2) {
+//     let sum = 0;
+//     for (const num of arguments) {
+//         sum = sum + num;
+//     }
 
-    return sum;
+//     return sum;
+// }
+// const sum = addNumber(23, 13, 50, 100);
+// // console.log(sum);
+
+
+// function getFullName(firstName, lastName) {
+//     let fullName = '';
+//     for (const part of arguments) {
+//         fullName = fullName + part + ' ';
+//     }
+//     return fullName;
+// }
+// const fullName = getFullName('go', 'hu', 'lo', 'koo');
+// console.log(fullName);
+
+
+// const add = (num1, num2) => num1 + num2;
+// const ans = add(20, 30);
+// console.log(ans);
+
+const doMath= (x, y) =>{
+    const sum=x+y;
+    const dff=x-y;
+    const result=sum*dff;
+    const output=result*8;
+    return output;
 }
-const sum = addNumber(23, 13, 50, 100);
-// console.log(sum);
-
-
-function getFullName(firstName, lastName) {
-    let fullName = '';
-    for (const part of arguments) {
-        fullName = fullName + part + ' ';
-    }
-    return fullName;
-}
-const fullName = getFullName('go', 'hu', 'lo', 'koo');
-console.log(fullName);
-
-
-const add = (num1, num2) => num1 + num2;
-const ans = add(20, 30);
-console.log(ans);
+const total =doMath(12, 8);
+console.log(total);
