c2ae820312c5d5f7c10596dff055b7d644af11b1
Ayed49 <ayedbabu60@gmail.com>
2024-03-18 23:49:05 +0600

    first commit

--- java.js
+++ java.js
@@ -1,10 +1,22 @@
-function loadPost(){
-    fetch('https://jasonplaceholder.typicode.com/posts')
-    .then(res =>res.json())
-    .then(data=> displayPosts(data))
-};
-loadPost();
+// function loadPost(){
+//     fetch('https://jsonplaceholder.typicode.com/users')
+//     .then(res =>res.json())
+//     .then(data=> displayPosts(data))
+// };
+// loadPost();
 
-function displayPosts(posts){
-    console.log(posts);
+// function displayPosts(posts){
+//     console.log(posts);
+// }
+
+
+
+const loadQuotes= ()=>{
+    fetch('https://api.kanye.rest/')
+    .then(res => res.json())
+    .then(data=>displayQuote(data) )
+}
+const displayQuote= quote =>{
+    const quoteElement=document.getElementById('quote');
+    quoteElement.innerText=quote.quote;
 }
