20871a81d0f48e23d58ee9abb5c3c7d8ce51d346
Ayed49 <ayedbabu60@gmail.com>
2024-03-19 16:17:48 +0600

    first commit

--- buddy.js
+++ buddy.js
@@ -1,11 +1,17 @@
 const loadBuddies = () =>{
-    fetch('https://randomuser.me/api/0.8/?results=10')
+    fetch('https://randomuser.me/api/?results=5')
     .then(res => res.json())
     .then(data =>displayBuddies(data));
 }
 loadBuddies();
 const displayBuddies = data =>{
     const buddies=data.results;
-    console.log(data.results);
     
+    const buddiesDiv=document.getElementById('buddies');
+    for(const buddy of buddies){
+        console.log(buddy.email);
+        const p=document.createElement('p');
+        p.innerText =buddy.email;
+        buddiesDiv.appendChild(p);
+    }
 }
