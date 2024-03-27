df38009f4e944befaae5167f7ab80dbafc245a59
Ayed49 <ayedbabu60@gmail.com>
2024-03-28 04:09:26 +0600

    first commit

--- buddy.js
+++ buddy.js
@@ -12,7 +12,7 @@ const searchFood =()=>{
 const displaySearchResult = meals =>{
     const searchResult = document.getElementById('search-result');
     meals.forEach(meal =>{
-        console.log(meal);
+        // console.log(meal);
 
         const div= document.createElement('div');
         div.classList.add('col');
@@ -33,4 +33,23 @@ const displaySearchResult = meals =>{
 
 const onclick= loadMealDetile = mealId =>{
   console.log(mealId);
+  const url = `https://www.themealdb.com/api/json/v1/1/lookup.php?i=${mealId}`;
+  fetch(url)
+  .then(res => res.json())
+  .then(data => displayMealDetails(data.meals[0]));
+}
+
+const displayMealDetails = meal =>{
+  const mealDetails= document.getElementById('meal-details');
+  const div = document.createElement('div');
+  div.classList.add('card');
+  div.innerHTML=`
+  <img src="${meal.strMealThumb}" class="card-img-top" alt="...">
+  <div class="card-body">
+    <h5 class="card-title">${meal.strMeal}</h5>
+    <p class="card-text">${meal.strInstructions.slice(0, 100)}</p>
+    <a href="${meal.strYoutube}" class="btn btn-primary">Go somewhere</a>
+  </div>
+  `;
+  mealDetails.appendChild(div);
 }
